fde866a2327db3c85e9f7b1655c9c989af48d3c9
pallobbiswas <pallobbiswas326@gmail.com>
2022-07-22 15:21:29 +0600

    blog donr

---
+++ src/components/blog/BlogLastPart.js
@@ -0,0 +1,330 @@
+import React from "react";
+import "../../styel/header.css";
+
+const BlogLastPart = () => {
+  return (
+    <div className="mt-4 mb-10">
+      <section className="md:px-32">
+        <div className="grid grid-cols-1 md:grid-cols-2">
+          <div className="mr-5">
+            <div>
+              <a href="#g" className="text-1xl font-bold text-blue-600">
+                June 1, 2022
+              </a>{" "}
+              <br />
+              <a
+                className="text-3xl font-bold
+              "
+                href="#g"
+              >
+                Pride at Udemy: It’s not trivial (but there is trivia)
+              </a>
+              <p className="mt-5">
+                When I started at Udemy in 2016 in San Francisco, I did what
+                most people do new group — I looked for my people Someone on my
+                hiring...
+              </p>
+              <img
+                className="my-5"
+                src="https://about.udemy.com/wp-content/uploads/2022/04/FNB-Thumbnail-1024x535.png"
+                alt=""
+              />
+            </div>
+            <div>
+              <a href="#g" className="text-1xl font-bold text-blue-600">
+                June 1, 2022
+              </a>{" "}
+              <br />
+              <a
+                className="text-3xl font-bold
+              "
+                href="#g"
+              >
+                Udemy Announces Partnership with South Africa’s First National
+                Bank
+              </a>
+              <p className="mt-5">
+                When I started at Udemy in 2016 in San Francisco, I did what
+                most people do new group — I looked for my people Someone on my
+                hiring...
+              </p>
+              <img
+                className="my-5"
+                src="https://about.udemy.com/wp-content/uploads/2022/03/Ukraine-2-1024x535.png"
+                alt=""
+              />
+            </div>
+            <div>
+              <a href="#g" className="text-1xl font-bold text-blue-600">
+                June 1, 2022
+              </a>{" "}
+              <br />
+              <a
+                className="text-3xl font-bold
+              "
+                href="#g"
+              >
+                Pride at Udemy: It’s not trivial (but there is trivia)
+              </a>
+              <p className="mt-5">
+                When I started at Udemy in 2016 in San Francisco, I did what
+                most people do new group — I looked for my people Someone on my
+                hiring...
+              </p>
+              <img
+                className="my-5"
+                src="https://about.udemy.com/wp-content/uploads/2022/01/Screen-Shot-2022-02-23-at-3.58.32-PM-1024x573.png"
+                alt=""
+              />
+            </div>
+            <div>
+              <a href="#g" className="text-1xl font-bold text-blue-600">
+                June 1, 2022
+              </a>{" "}
+              <br />
+              <a
+                className="text-3xl font-bold
+              "
+                href="#g"
+              >
+                Pride at Udemy: It’s not trivial (but there is trivia)
+              </a>
+              <p className="mt-5">
+                When I started at Udemy in 2016 in San Francisco, I did what
+                most people do new group — I looked for my people Someone on my
+                hiring...
+              </p>
+              <img
+                className="my-5"
+                src="https://about.udemy.com/wp-content/uploads/2022/04/FNB-Thumbnail-1024x535.png"
+                alt=""
+              />
+            </div>
+            <div>
+              <a href="#g" className="text-1xl font-bold text-blue-600">
+                June 1, 2022
+              </a>{" "}
+              <br />
+              <a
+                className="text-3xl font-bold
+              "
+                href="#g"
+              >
+                Pride at Udemy: It’s not trivial (but there is trivia)
+              </a>
+              <p className="mt-5">
+                When I started at Udemy in 2016 in San Francisco, I did what
+                most people do new group — I looked for my people Someone on my
+                hiring...
+              </p>
+              <img
+                className="my-5"
+                src="https://about.udemy.com/wp-content/themes/wp-about-v4/assets/images/fallbackimage.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <a href="#g" className="text-1xl font-bold text-blue-600">
+                June 1, 2022
+              </a>{" "}
+              <br />
+              <a
+                className="text-3xl font-bold
+              "
+                href="#g"
+              >
+                Pride at Udemy: It’s not trivial (but there is trivia)
+              </a>
+              <p className="mt-5">
+                When I started at Udemy in 2016 in San Francisco, I did what
+                most people do new group — I looked for my people Someone on my
+                hiring...
+              </p>
+              <img
+                className="my-5"
+                src="https://about.udemy.com/wp-content/uploads/2021/10/TURBO-ANIMATION-CONCEPT.gif"
+                alt=""
+              />
+              <a href="#g" className="text-1xl font-bold text-blue-600">
+                June 1, 2022
+              </a>{" "}
+              <br />
+              <a
+                className="text-3xl font-bold
+              "
+                href="#g"
+              >
+                Pride at Udemy: It’s not trivial (but there is trivia)
+              </a>
+              <p className="mt-5">
+                When I started at Udemy in 2016 in San Francisco, I did what
+                most people do new group — I looked for my people Someone on my
+                hiring...
+              </p>
+            </div>
+          </div>
+          {/* second one */}
+          <div className="ml-5">
+            <div>
+              <a href="#g" className="text-1xl font-bold text-blue-600">
+                June 1, 2022
+              </a>{" "}
+              <br />
+              <a
+                className="text-3xl font-bold
+              "
+                href="#g"
+              >
+                Further Embedding Belonging, Equity, Diversity, and Inclusion (BEDI) at Udemy
+              </a>
+              <p className="mt-5">
+                When I started at Udemy in 2016 in San Francisco, I did what
+                most people do new group — I looked for my people Someone on my
+                hiring...
+              </p>
+              <img
+                className="my-5"
+                src="https://about.udemy.com/wp-content/uploads/2022/03/LinkedIn-1024x683.png"
+                alt=""
+              />
+            </div>
+            <div>
+              <a href="#g" className="text-1xl font-bold text-blue-600">
+                June 1, 2022
+              </a>{" "}
+              <br />
+              <a
+                className="text-3xl font-bold
+              "
+                href="#g"
+              >
+                Udemy Announces Partnership with South Africa’s First National
+                Bank
+              </a>
+              <p className="mt-5">
+                When I started at Udemy in 2016 in San Francisco, I did what
+                most people do new group — I looked for my people Someone on my
+                hiring...
+              </p>
+              <img
+                className="my-5"
+                src="https://about.udemy.com/wp-content/uploads/2022/03/Untitled-1200-×-400-px-1-1024x341.png"
+                alt=""
+              />
+            </div>
+            <div>
+              <a href="#g" className="text-1xl font-bold text-blue-600">
+                June 1, 2022
+              </a>{" "}
+              <br />
+              <a
+                className="text-3xl font-bold
+              "
+                href="#g"
+              >
+                Women’s History Month: Hear how Udemy enables opportunities for women everywhere
+              </a>
+              <p className="mt-5">
+                When I started at Udemy in 2016 in San Francisco, I did what
+                most people do new group — I looked for my people Someone on my
+                hiring...
+              </p>
+              <img
+                className="my-5"
+                src="https://about.udemy.com/wp-content/uploads/2022/02/Blog-Post-HBCUs-2-1024x535.png"
+                alt=""
+              />
+            </div>
+            <div>
+              <a href="#g" className="text-1xl font-bold text-blue-600">
+                June 1, 2022
+              </a>{" "}
+              <br />
+              <a
+                className="text-3xl font-bold
+              "
+                href="#g"
+              >
+                The History of Historically Black Colleges and Universities (HBCUs)
+              </a>
+              <p className="mt-5">
+                When I started at Udemy in 2016 in San Francisco, I did what
+                most people do new group — I looked for my people Someone on my
+                hiring...
+              </p>
+              <img
+                className="my-5"
+                src="https://about.udemy.com/wp-content/uploads/2021/11/SOF_full_color_logo_HOR.png-1024x348.png"
+                alt=""
+              />
+            </div>
+            <div>
+              <a href="#g" className="text-1xl font-bold text-blue-600">
+                June 1, 2022
+              </a>{" "}
+              <br />
+              <a
+                className="text-3xl font-bold
+              "
+                href="#g"
+              >
+                Pride at Udemy: It’s not trivial (but there is trivia)
+              </a>
+              <p className="mt-5">
+                When I started at Udemy in 2016 in San Francisco, I did what
+                most people do new group — I looked for my people Someone on my
+                hiring...
+              </p>
+              <img
+                className="my-5"
+                src="https://about.udemy.com/wp-content/themes/wp-about-v4/assets/images/fallbackimage.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <a href="#g" className="text-1xl font-bold text-blue-600">
+                June 1, 2022
+              </a>{" "}
+              <br />
+              <a
+                className="text-3xl font-bold
+              "
+                href="#g"
+              >
+                Pride at Udemy: It’s not trivial (but there is trivia)
+              </a>
+              <p className="mt-5">
+                When I started at Udemy in 2016 in San Francisco, I did what
+                most people do new group — I looked for my people Someone on my
+                hiring...
+              </p>
+              <img
+                className="my-5"
+                src="https://about.udemy.com/wp-content/themes/wp-about-v4/assets/images/fallbackimage.jpg"
+                alt=""
+              />
+              <a href="#g" className="text-1xl font-bold text-blue-600">
+                June 1, 2022
+              </a>{" "}
+              <br />
+              <a
+                className="text-3xl font-bold
+              "
+                href="#g"
+              >
+                Pride at Udemy: It’s not trivial (but there is trivia)
+              </a>
+              <p className="mt-5">
+                When I started at Udemy in 2016 in San Francisco, I did what
+                most people do new group — I looked for my people Someone on my
+                hiring...
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default BlogLastPart;
--- src/components/blog/Blogs.js
+++ src/components/blog/Blogs.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Footer from '../shared/Footer';
 import Header from '../shared/Header';
 import BlogBanner from './BlogBanner';
+import BlogLastPart from './BlogLastPart';
 
 const Blogs = () => {
     return (
         <div>
             <Header>
                 <BlogBanner />
+                <BlogLastPart />
                 <Footer />
             </Header>
         </div>
